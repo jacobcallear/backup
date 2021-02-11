472984e97e4c401e66ee9e5b251999265b812e85
Jacob Callear <jacob18@callear.net>
2021-02-11 14:26:49 +0000

    Enable pip install from local files

---
+++ backup/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='backup',
+    version='0.1.0',
+    url='https://github.com/jacobcallear/backup',
+    author='Jacob Callear',
+    author_email='jacob18@callear.net',
+    description='Backup your folders with python.',
+    packages=find_packages(),
+    install_requires=[]
+)
